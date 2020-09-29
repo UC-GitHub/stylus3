3edf7de15e338005c80b6433d98df1cd32841d48
UC-GitHub <yasutsugu58@gmail.com>
2020-09-29 16:40:19 +0900

    ActiveHash作成

---
+++ app/models/style.rb
@@ -0,0 +1,17 @@
+class Style < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :type
+  belongs_to_active_hash :season
+  belongs_to_active_hash :height
+  belongs_to_active_hash :body_figure
+  belongs_to_active_hash :shipping_date
+
+  VALID_PRICE_REGEX = /\A[-]?[0-9]+(\.[0-9]+)?\z/
+  
+  validates :image, :name, :description, :tops1_price, :bottoms1_price, :total_price, presence: true
+  validates :type_id, :season_id, :height_id, :body_figure_id, :shipping_date_id, numericality: { other_than: 1, message: 'must be Selected' }
+
+  has_one :order
+  belongs_to :supplier
+  has_one_attached :image
+end
