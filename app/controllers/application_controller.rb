e9ac0c26443eecdfa9ef809226ced065776f0511
UC-GitHub <yasutsugu58@gmail.com>
2020-09-29 16:03:11 +0900

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :shoe_size,
+                                                      :display_name, :company_name, :family_name_in_charge, :first_name_in_charge, :kana_family_name_in_charge, :kana_first_name_in_charge, :mobile_number])
+  end
+
 end
