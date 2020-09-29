4956f04bef0879b4bb15b9fe9a9f1c3bb76cb43c
UC-GitHub <yasutsugu58@gmail.com>
2020-09-29 16:40:39 +0900

    モデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_071958) do
+ActiveRecord::Schema.define(version: 2020_09_29_073626) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "styles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "type_id", null: false
+    t.integer "season_id", null: false
+    t.integer "height_id", null: false
+    t.integer "body_figure_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.text "description", null: false
+    t.integer "tops1_price", null: false
+    t.integer "tops2_price"
+    t.integer "tops3_price"
+    t.integer "tops4_price"
+    t.integer "tops5_price"
+    t.integer "bottoms1_price", null: false
+    t.integer "bottoms2_price"
+    t.integer "shoes_price"
+    t.integer "accessories1_price"
+    t.integer "accessories2_price"
+    t.integer "accessories3_price"
+    t.integer "total_price", null: false
+    t.bigint "supplier_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["supplier_id"], name: "index_styles_on_supplier_id"
+  end
 
   create_table "suppliers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "display_name", null: false
@@ -62,4 +109,6 @@ ActiveRecord::Schema.define(version: 2020_09_28_071958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "styles", "suppliers"
 end
